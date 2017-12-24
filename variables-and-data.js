138b56bcacb8195483d2c6e0339a7e1c279e32cb
Tristan Bennett <tmbennett2011@msn.com>
2017-12-24 13:13:12 -0700

    more obj variables and operators

--- variables-and-data.js
+++ variables-and-data.js
@@ -26,3 +26,39 @@ obj = {
 console.log(obj.obj2.obj2key1);
 */
 
+/*
+obj = {
+	key1: "value",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2key1: "internal object value",
+	}
+};
+var war;
+war = "this is war....I think!";
+console.log(typeof war);
+
+*/
+
+/*
+variable = "2";         		 //dynamic type
+variable2 = 3;
+
+var1 = "2";
+var2 = 5;
+result = var1 + var2;
+
+console.log(result);							//not intended result?
+*/
+
+add = 1+ 2;
+sub = 2 - 1;
+mult = 2 * 4;
+division = 4 / 2;
+mod = 5 % 2;										//modulo returns remainder of division
+
+string = "string 1" + " " + "string 2";
+
+console.log("what is the remainder of 5/2?");
+console.log(mod);
