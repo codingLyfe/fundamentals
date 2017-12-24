eda2f19e12b3c0a8a7c6582e6549d1ba0d235005
Tristan Bennett <tmbennett2011@msn.com>
2017-12-24 15:02:35 -0700

    created functions to hold if statement. passed params to functions

--- conditionals-and-functions.js
+++ conditionals-and-functions.js
@@ -28,13 +28,25 @@ notEquals = 5 !== 2;
 storeA = 1.40;
 storeB = 3.40;
 
-storeAIsLower = storeA < storeB;
+function compareStorePrices (storeA, storeB){
+	storeAIsLower = storeA < storeB;
 
-if (storeAIsLower) {
-	console.log("Store A has a lower price.")
-} else if (storeB < storeA) {
-	console.log("Store B has a lower price.")
-} else {
-	console.log("Their prices are equal.")
+	if(storeAIsLower) {
+		console.log("Store A has a lower price.")
+	} else if(storeB < storeA) {
+		console.log("Store B has a lower price.")
+	} else {
+		console.log("Their prices are equal.")
+	}
 }
 
+compareStorePrices(10,5);
+//compareStorePrices(7,10);
+
+function squareNum(number) {
+	squared = number * number;
+	return squared;
+}
+
+squaredNumber = squareNum(4);
+console.log(squaredNumber);
