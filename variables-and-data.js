3345a8b4dc6771ce6590f1033e3a383d98f017cc
Tristan Bennett <tmbennett2011@msn.com>
2017-12-24 12:45:40 -0700

    explored basic and composite variables

--- variables-and-data.js
+++ variables-and-data.js
@@ -2,5 +2,27 @@
 	Variables, Data Types, and Typing
  */
 
-let wordData = "Word list";
+/*
+var wordData;
+
+wordData = "Word Data";
+
+console.log(wordData);
+
+subTotal= 100;
+tax = subTotal * 0.07;
+totalWithTax = tax + subTotal;
+*/
+
+/*
+obj = {
+	key1: "Value 1",
+	key2: 3,
+	obj2: {
+		obj2key1: "value of inner object",
+	}
+};
+
+console.log(obj.obj2.obj2key1);
+*/
 
